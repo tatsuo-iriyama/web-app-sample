3f5b90dfe69222d64909bea71ede10c194812f43
tatsuo-iriyama <tatsuo.iriyama@yourmystar.jp>
2019-08-11 22:48:13 +0900

    ルーティング修正と追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-    get '/users/input', to: 'users#input'
+    get '/users/complete/', to: 'useres#complete'
+    get '/users/input/', to: 'users#input'
     get '/users/', to: 'users#index'
     get '/', to: 'root#index'
-    post '/users/register', to: 'users#register'
+    post '/users/register/', to: 'users#register'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
